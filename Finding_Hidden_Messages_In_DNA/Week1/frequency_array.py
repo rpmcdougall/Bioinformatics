530aabb4cd342e63483f3af189ddf5eac1d6edf6
Ryan McDougall <rp.mcdougall@gmail.com>
2020-09-15 22:55:43 -0500

    finished clumping, need to go back and speed it up for code submission using dict

--- Finding_Hidden_Messages_In_DNA/Week1/frequency_array.py
+++ Finding_Hidden_Messages_In_DNA/Week1/frequency_array.py
@@ -8,14 +8,13 @@ def NumberToSymbol(symbol):
     symbol_map = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
     return symbol_map.get(symbol)
 
-# TODO: This isn't even close, read the prompt dummy
-
-
-def NumberToPattern(Number):
-    pattern = []
-    for char in str(Number):
-        pattern.append(NumberToSymbol(int(char)))
-    return ''.join([str(x) for x in pattern])
+def NumberToPattern(index, k):
+    if k == 1:
+        return NumberToSymbol(index)
+    prefix_index, r = divmod(index, 4)
+    symbol = NumberToSymbol(r)
+    prefix_pattern = NumberToPattern(prefix_index, k - 1)
+    return prefix_pattern + symbol
 
 
 def PatternToNumber(Pattern):
@@ -42,16 +41,22 @@ def ClumpFinding(Genome, k, L, t):
     Clump = []
     for i in range(0, 4 ** k - 1):
         Clump.append(0)
-    for i in range(0, len(Genome) - L):
-        Text = Genome[i: i + L]
-        FrequencyArray = ComputingFrequencies(Text, k)
-        for j in range(0, 4 ** k - 1):
-            if FrequencyArray[j] >= t:
-                Clump[j] = 1
+    Text = Genome[0:L]
+    FrequencyArray = ComputingFrequencies(Text, k)
+    for i in range(0, 4 ** k - 1):
+        if FrequencyArray[i] >= t:
+            Clump[i] = 1
+    for i in range(1, len(Genome) - L):
+       FirstPattern = Genome[i-1:i -1 + k]
+       index = PatternToNumber(FirstPattern)
+       FrequencyArray[index] = FrequencyArray[index] - 1
+       LastPattern = Genome[i+L-k:i + L]
+       index = PatternToNumber(LastPattern)
+       FrequencyArray[index] = FrequencyArray[index] + 1
     for i in range(0, 4 ** k - 1):
         if Clump[i] == 1:
             FrequentPatterns.append(NumberToPattern(i, k))
     return FrequentPatterns
 
 
-print(NumberToPattern("5437"))
+print(len(ClumpFinding(open("E_coli.txt", "r").read(), 9, 500, 3)))
